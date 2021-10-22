bea4f7652459fd6f87592565fff65567286089fb
Deedss <gertjan.rolink@outlook.com>
2021-10-22 15:28:37 +0200

    update

--- scripts/.scripts/install.sh
+++ scripts/.scripts/install.sh
@@ -54,7 +54,10 @@ function main_packages(){
         ncurses \
         git \
         curl \
-        wget
+        wget \
+        zstd \
+        openssl \
+
 }
 
 function install_oh_my_zsh(){
@@ -85,4 +88,8 @@ function install_kitty(){
     cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/ \
     # Update the path to the kitty icon in the kitty.desktop file
     sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty.desktop
+}
+
+function install_flathub(){
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 }
