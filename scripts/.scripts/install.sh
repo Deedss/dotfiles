af0cb1813b8d1932e2b8814b244cd3f531525ba9
Deedss <gertjan.rolink@outlook.com>
2021-10-24 21:03:34 +0200

    update

--- scripts/.scripts/install.sh
+++ scripts/.scripts/install.sh
@@ -106,3 +106,13 @@ function install_neovim(){
     chmod u+x nvim.appimage
     sudo ln -s ~/Tools/AppImages/nvim.appimage /usr/local/bin/nvim
 }
+
+function install_vscode(){
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    sudo dnf install code
+}
+
+function install_pythontools(){
+    pip install virtualenvwrapper
+}
