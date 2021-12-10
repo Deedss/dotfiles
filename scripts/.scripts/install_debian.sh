fbc00633090217383d0003511b9e2553bc5821f4
Deedss <gertjan.rolink@outlook.com>
2021-12-10 14:03:59 +0100

    added install for debian

---
+++ scripts/.scripts/install_debian.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+# This file contains most things that I run while installing the main fedora-kde
+# install
+
+function main_packages(){
+    sudo apt install -y 
+
+}
+
+function install_oh_my_zsh(){
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+        git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+}
+
+function install_rust(){
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+}
+
+function install_brave(){
+    sudo apt install apt-transport-https curl
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt update
+    sudo apt install brave-browser
+}
+
+function install_kitty(){
+    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+    # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
+    # your PATH)
+    ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+    # Place the kitty.desktop file somewhere it can be found by the OS
+    cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+    # Update the path to the kitty icon in the kitty.desktop file
+    sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty.desktop
+}
+
+function install_flathub(){
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+}
+
+function install_neovim(){
+    mkdir -p /Tools/AppImages
+    wget -P ~/Tools/AppImages/ https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+    chmod u+x ~/Tools/AppImages/nvim.appimage
+    sudo cp ~/Tools/AppImages/nvim.appimage /usr/local/bin/nvim
+    sudo chown $USER:$USER /usr/local/bin/nvim
+}
+
+function install_vscode(){
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
+    sudo apt install apt-transport-https
+    sudo apt update
+    sudo apt install code # or code-insiders
+}
+
+function install_pythontools(){
+    sudo apt install python3-dev python3-wheel python3-venv
+    pip install virtualenvwrapper
+}
+
+function install_docker(){
+    sudo apt-get install ca-certificates curl gnupg lsb-release
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+    sudo usermod -aG docker $USER
+    sudo systemctl enable docker.service
+    sudo systemctl enable containerd.service
+}
+
+function install_npm(){
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+    sudo apt install nodejs
+    sudo npm install -g pyright
+}
+
+function install_spotify(){
+    curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt-get update && sudo apt-get install spotify-client
+}
+
+function install_flutter(){
+    mkdir -p ~/Tools
+    cd ~/Tools
+    git clone https://github.com/flutter/flutter.git -b stable
+    flutter doctor
+}
+
+function install_espIdf(){
+    sudo apt-get install git wget flex bison gperf python3 python3-pip python3-setuptools cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+    mkdir -p ~/Tools
+    cd ~/Tools
+    git clone --recursive https://github.com/espressif/esp-idf.git
+    cd esp-idf
+    sh install.sh
+}
+
+function install_emscripten(){
+    mkdir -p ~/Tools
+    cd ~/Tools
+    # Get the emsdk repo
+    git clone https://github.com/emscripten-core/emsdk.git
+    # Enter that directory
+    cd emsdk
+    # Fetch the latest version of the emsdk (not needed the first time you clone)
+    git pull
+    # Download and install the latest SDK tools.
+    ./emsdk install latest
+    # Make the "latest" SDK "active" for the current user. (writes .emscripten file)
+    ./emsdk activate latest
+}
