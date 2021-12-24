67f85eae30e3be5bd2feebcf9db03463d11d07ae
Deedss <gertjan.rolink@outlook.com>
2021-12-24 20:40:04 +0100

    update

--- scripts/.scripts/install.sh
+++ scripts/.scripts/install.sh
@@ -77,6 +77,8 @@ function install_oh_my_zsh(){
 
 function install_rust(){
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+    chmod +x ~/.local/bin/rust-analyzer
     sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 }
 
