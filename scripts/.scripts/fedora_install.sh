596e5647948ae92f5eed6b7e3de1c37baaf51035
Gertjan <gertjan.rolink@outlook.com>
2023-04-29 22:53:53 +0200

    add gnome stuff

--- scripts/.scripts/fedora_install.sh
+++ scripts/.scripts/fedora_install.sh
@@ -1,6 +1,69 @@
 #!/bin/bash
 source tools_install.sh
 
+function install-gnome(){
+    echo "Perform Installation for Fedora KDE"
+    setup-dnf
+
+    ### Cleanup GNOME
+    clean-gnome
+
+    ### Generic Setup
+    install-rpmfusion
+    default-packages
+    install-brave
+    install-vscode
+    install-pythontools
+    install-rust
+    install-oh-my-zsh
+    install-podman
+
+    ##### FLATPAKS
+    install-flatpak
+}
+
+###############################################################################
+###  INSTALLATION GNOME                                                     ###
+###############################################################################
+function install-gnome(){
+    echo "Perform Installation for Fedora Gnome"
+    ### Set the correct DNF Settings
+    setup-dnf
+
+    ### Clean up GNOME
+    clean-gnome
+
+    ### Generic Setup
+    install-rpmfusion
+    default-packages
+    install-brave
+    install-vscode
+    install-pythontools
+    install-rust
+    install-oh-my-zsh
+    install-podman
+
+    ##### FLATPAKS
+    install-flatpak
+}
+
+###############################################################################
+###  CLEAN UP GNOME                                                         ###
+###############################################################################
+function clean-gnome(){
+    ### Clean up GNOME packages
+    sudo dnf autoremove -y \
+        gnome-tour gnome-boxes libreoffice-* \
+        gnome-weather gnome-maps totem mediawriter \
+        gnome-connections gnome-software firefox \
+        gnome-calendar gnome-initial-setup gnome-contacts \
+        gnome-classic-session
+
+    ## Install for Gnome specific
+    sudo dnf install -y \
+        adwaita-gtk2-theme evolution evolution-ews
+}
+
 ###############################################################################
 ###  INSTALLATION KDE                                                       ###
 ###############################################################################
@@ -112,14 +175,25 @@ function install-flatpak(){
     io.podman_desktop.PodmanDesktop
 
     ##### KDE #####
-    flatpak install -y \
-    org.wezfurlong.wezterm \
-    org.kde.okular \
-    org.kde.gwenview \
-    org.kde.kcalc \
-    org.gnome.Evolution \
-    org.gtk.Gtk3theme.Arc-Dark \
-    org.gtk.Gtk3theme.Arc-Dark-solid
+    if [[ "$XDG_SESSION_DESKTOP" == "KDE" ]];
+    then
+        flatpak install -y \
+        org.wezfurlong.wezterm \
+        org.kde.okular \
+        org.kde.gwenview \
+        org.kde.kcalc \
+        org.gnome.Evolution \
+        org.gtk.Gtk3theme.Arc-Dark \
+        org.gtk.Gtk3theme.Arc-Dark-solid
+    fi
+
+    ##### GNOME #####
+    if [[ "$XDG_SESSION_DESKTOP" == "gnome" ]];
+    then
+        flatpak install -y \
+        org.gtk.Gtk3theme.Adwaita-dark \
+        org.gtk.Gtk3theme.adw-gtk3-dark
+    fi
 }
 
 ###############################################################################
