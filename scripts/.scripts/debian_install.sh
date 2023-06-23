72aff8ba08d31bc1e93c1fb229fcdfbf6ee93bf2
Gertjan <gertjan.rolink@outlook.com>
2023-06-23 23:24:20 +0200

    add stuff for debian

--- scripts/.scripts/debian_install.sh
+++ scripts/.scripts/debian_install.sh
@@ -32,7 +32,7 @@ function install-kde(){
 function install-kde-desktop(){
     echo "Install kde desktop"
     sudo apt -y install plasma-desktop plasma-workspace plasma-nm \
-        kdialog kfind kde-spectacle libpam-kwallet5 \
+        kdialog kfind kde-spectacle libpam-kwallet5 kde-config-flatpak \
         udisks2 upower kwin-x11 kwin-wayland sddm xserver-xorg
 }
 
@@ -124,7 +124,7 @@ function install-brave(){
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
     sudo apt update
-    sudo apt install brave-browser
+    sudo apt install -y brave-browser
 }
 
 ###############################################################################
@@ -137,6 +137,8 @@ function install-vscode(){
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
     rm -f packages.microsoft.gpg
+    sudo apt update
+    sudo apt install -y code
 }
 
 ###############################################################################
