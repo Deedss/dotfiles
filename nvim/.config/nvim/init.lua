be1b97e6d7b2e25101ade71b8f06901dd1fc8a7a
Gertjan <gertjan.rolink@outlook.com>
2023-08-03 22:28:24 +0200

    use lazyvim for a bit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,22 +1,2 @@
--- LOAD PACKER AND PACKER PLUGINS
-require "first_load"
-require "plugins"
-
--- LOAD SETTINGS AND KEYMAPS
-require "options"
-require "keymaps"
-require "commands"
-require "user-interface"
-
--- LOAD PLUGINS
-require "plugin.telescope-config"
-require "plugin.treesitter-config"
-
--- LOAD LSP-config
-require "lsp.mason"
-require "lsp.lsp"
-require "lsp.nvim-cmp-config"
-require "lsp.null-ls"
-
--- LOAD DAP config
-require "dap.dap"
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
