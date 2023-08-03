036c5d12b5db789a040e06a1b99e0d89ea814d45
Gertjan <gertjan.rolink@outlook.com>
2023-08-03 06:59:41 +0200

    Revert "reove gnome"
    This reverts commit 9e46eb7831235b8c3b37b193ccaccdbc4e2bf02f.

--- scripts/.scripts/fedora_install.sh
+++ scripts/.scripts/fedora_install.sh
@@ -18,13 +18,33 @@ function install-desktop(){
     install-oh-my-zsh
     install-podman
     install-iwd
-    install-emscripten
-    
-    ### THEME
-    install-arc-theme
+
+    ### THEME FOR KDE
+    if [[ "$XDG_SESSION_DESKTOP" == "KDE" ]];
+    then
+        install-emscripten
+        install-arc-theme
+    fi
 
     ##### FLATPAKS
     install-flatpak
+}
+
+###############################################################################
+###  CLEAN UP GNOME                                                         ###
+###############################################################################
+function clean-gnome(){
+    ### Clean up GNOME packages
+    sudo dnf autoremove -y \
+        gnome-tour gnome-boxes libreoffice-* \
+        gnome-weather gnome-maps totem mediawriter \
+        gnome-connections gnome-software firefox \
+        gnome-calendar gnome-initial-setup gnome-contacts \
+        gnome-classic-session
+
+    ## Install for Gnome specific
+    sudo dnf install -y \
+        adwaita-gtk2-theme
 }
 
 ###############################################################################
@@ -116,17 +136,28 @@ function install-flatpak(){
     org.blender.Blender \
     org.videolan.VLC \
     org.freedesktop.Platform.ffmpeg-full \
-    io.podman_desktop.PodmanDesktop \
+    io.podman_desktop.PodmanDesktop
 
     ##### KDE #####
-    flatpak install -y \
-    org.wezfurlong.wezterm \
-    org.kde.okular \
-    org.kde.gwenview \
-    org.kde.kcalc \
-    org.gnome.Evolution \
-    org.gtk.Gtk3theme.Arc-Dark \
-    org.gtk.Gtk3theme.Arc-Dark-solid
+    if [[ "$XDG_SESSION_DESKTOP" == "KDE" ]];
+    then
+        flatpak install -y \
+        org.wezfurlong.wezterm \
+        org.kde.okular \
+        org.kde.gwenview \
+        org.kde.kcalc \
+        org.gnome.Evolution \
+        org.gtk.Gtk3theme.Arc-Dark \
+        org.gtk.Gtk3theme.Arc-Dark-solid
+    fi
+
+    ##### GNOME #####
+    if [[ "$XDG_SESSION_DESKTOP" == "gnome" ]];
+    then
+        flatpak install -y \
+        org.gtk.Gtk3theme.Adwaita-dark \
+        org.gtk.Gtk3theme.adw-gtk3-dark
+    fi
 }
 
 ###############################################################################
