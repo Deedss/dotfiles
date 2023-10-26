a5ac56e6ce097634b8606b756c46e3f16383bc69
Gertjan <gertjan.rolink@outlook.com>
2023-10-26 09:21:07 +0200

    add helix

--- scripts/.scripts/install/tools_install.sh
+++ scripts/.scripts/install/tools_install.sh
@@ -219,3 +219,25 @@ function install-neovim(){
     cd ~ || exit
     echo ''
 }
+
+###############################################################################
+##### HELIX                                                             #######
+###############################################################################
+function install-helix(){
+    echo "Install Helix"
+    cd ~/.local/bin || exit
+    DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/helix-editor/helix/releases/latest" | jq -r '.assets[] | select(.name | endswith("-x86_64.AppImage")).browser_download_url')
+    if [ "$DOWNLOAD_URL" ]; then
+        curl -L -o helix "$DOWNLOAD_URL" && chmod +x helix
+    fi
+    cd ~ || exit
+    echo ''
+}
+
+##############################################################################
+##### KDE CONFIG                                                        ######
+##############################################################################
+function kde-configuration(){
+    # Have Meta key open Overview
+    kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.kglobalaccel,/component/kwin,org.kde.kglobalaccel.Component,invokeShortcut,Overview"
+}
