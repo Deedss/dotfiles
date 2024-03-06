2088f4bad01e79c180575994a852e2e46c8106eb
Gertjan Rolink <gertjan.rolink@outlook.com>
2024-03-06 18:59:57 +0100

    update

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -2,17 +2,11 @@ return { -- Collection of various small independent plugins/modules
 	"echasnovski/mini.nvim",
 	config = function()
 		-- Better Around/Inside textobjects
-		--
 		-- Examples:
 		--  - va)  - [V]isually select [A]round [)]parenthen
 		--  - yinq - [Y]ank [I]nside [N]ext [']quote
 		--  - ci'  - [C]hange [I]nside [']quote
 		require("mini.ai").setup({ n_lines = 500 })
-
-		-- Simple and easy statusline.
-		--  You could remove this setup call if you don't like it,
-		--  and try some other statusline plugin
-		-- require("mini.statusline").setup()
 
 		-- Simple and easy tablin
 		require("mini.tabline").setup()
@@ -23,8 +17,5 @@ return { -- Collection of various small independent plugins/modules
 		-- down = '<M-j>',
 		-- up = '<M-k>',
 		require("mini.move").setup()
-
-		-- ... and there is more!
-		--  Check out: https://github.com/echasnovski/mini.nvim
 	end,
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -11,6 +11,16 @@ return { -- Highlight, edit, and navigate code
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
+            },
         })
 
         -- There are additional nvim-treesitter modules that you can use to interact
