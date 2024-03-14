914442052cdecf9a94898bb68c7e5722510d1bc7
Gertjan Rolink <gertjan.rolink@outlook.com>
2024-03-14 17:49:25 +0100

    improved config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,92 +1,88 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
+local config = {}
 
-return {
-	font_size = 10.0,
-	-- color_scheme = "OneDark (base16)",
-	warn_about_missing_glyphs = false,
-	check_for_updates = false,
+config.check_for_updates = false
+config.warn_about_missing_glyphs = false
+config.window_close_confirmation = "NeverPrompt"
 
-	window_close_confirmation = "NeverPrompt",
+config.initial_rows = 30
+config.initial_cols = 110
 
-	initial_rows = 30,
-	initial_cols = 110,
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 
-	---------------------------------------------
-	------- TAB BAR SETTINGS --------------------
-	---------------------------------------------
-	use_fancy_tab_bar = false,
-	tab_bar_at_bottom = true,
+config.font_size = 10.0
 
-	-- Tab bar colour schemes
-	colors = {
-		-- # GitHub Dimmed
-		-- [colors]
-		foreground = "#adbac7",
-		background = "#22272e",
-		cursor_bg = "#6cb6ff",
-		cursor_border = "#6cb6ff",
-		cursor_fg = "#101216",
-		selection_bg = "#264466",
-		selection_fg = "#ffffff",
+-- Tab bar colour schemes
+config.colors = {
+	-- # GitHub Dimmed
+	-- [colors]
+	foreground = "#adbac7",
+	background = "#22272e",
+	cursor_bg = "#6cb6ff",
+	cursor_border = "#6cb6ff",
+	cursor_fg = "#101216",
+	selection_bg = "#264466",
+	selection_fg = "#ffffff",
 
-		ansi = { "#545d68", "#f47067", "#57ab5a", "#c69026", "#539bf5", "#b083f0", "#39c5cf", "#909dab" },
-		brights = { "#636e7b", "#ff938a", "#6bc46d", "#daaa3f", "#6cb6ff", "#dcbdfb", "#56d4dd", "#cdd9e5" },
-		tab_bar = {
-			background = "#282c34",
-			active_tab = {
-				bg_color = "#282c34",
-				fg_color = "#ffffff",
-				intensity = "Bold",
-				italic = true,
-			},
-			inactive_tab = {
-				bg_color = "#282c34",
-				fg_color = "#a0a0a0",
-				intensity = "Half",
-				italic = false,
-			},
-			new_tab = {
-				bg_color = "#282c34",
-				fg_color = "#a0a0a0",
-			},
+	ansi = { "#545d68", "#f47067", "#57ab5a", "#c69026", "#539bf5", "#b083f0", "#39c5cf", "#909dab" },
+	brights = { "#636e7b", "#ff938a", "#6bc46d", "#daaa3f", "#6cb6ff", "#dcbdfb", "#56d4dd", "#cdd9e5" },
+	tab_bar = {
+		background = "#282c34",
+		active_tab = {
+			bg_color = "#282c34",
+			fg_color = "#ffffff",
+			intensity = "Bold",
+			italic = true,
 		},
-	},
-	-----------------------------------------
-	-------- KEY MAPPINGS -------------------
-	-----------------------------------------
-	keys = {
-		-- vertical split
-		{
-			key = "Enter",
-			mods = "CTRL|SHIFT|ALT",
-			action = act.SplitVertical,
+		inactive_tab = {
+			bg_color = "#282c34",
+			fg_color = "#a0a0a0",
+			intensity = "Half",
+			italic = false,
 		},
-		-- This will create a new split and run your default program inside it
-		{
-			key = "Enter",
-			mods = "CTRL|SHIFT",
-			action = act.SplitHorizontal,
-		},
-		{
-			key = "H",
-			mods = "CTRL|SHIFT",
-			action = act.ActivatePaneDirection("Left"),
-		},
-		{
-			key = "L",
-			mods = "CTRL|SHIFT",
-			action = act.ActivatePaneDirection("Right"),
-		},
-		{
-			key = "K",
-			mods = "CTRL|SHIFT",
-			action = act.ActivatePaneDirection("Up"),
-		},
-		{
-			key = "J",
-			mods = "CTRL|SHIFT",
-			action = act.ActivatePaneDirection("Down"),
+		new_tab = {
+			bg_color = "#282c34",
+			fg_color = "#a0a0a0",
 		},
 	},
 }
+-----------------------------------------
+-------- KEY MAPPINGS -------------------
+-----------------------------------------
+config.keys = {
+	-- vertical split
+	{
+		key = ")",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitVertical,
+	},
+	-- This will create a new split and run your default program inside it
+	{
+		key = "(",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitHorizontal,
+	},
+	{
+		key = "H",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "L",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "K",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "J",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+}
+
+return config
