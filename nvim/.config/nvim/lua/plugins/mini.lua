2b212ab6d8700d3f46550bb9acf5464fd7a17b63
Gertjan Rolink <gertjan.rolink@outlook.com>
2024-03-21 13:48:29 +0100

    add barbar

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,16 +1,6 @@
 return { -- Collection of various small independent plugins/modules
 	"echasnovski/mini.nvim",
 	config = function()
-		-- Better Around/Inside textobjects
-		-- Examples:
-		--  - va)  - [V]isually select [A]round [)]parenthen
-		--  - yinq - [Y]ank [I]nside [N]ext [']quote
-		--  - ci'  - [C]hange [I]nside [']quote
-		require("mini.ai").setup({ n_lines = 500 })
-
-		-- Simple and easy tablin
-		require("mini.tabline").setup()
-
 		-- Movement for lines
 		-- left = '<M-h>',
 		-- right = '<M-l>',
