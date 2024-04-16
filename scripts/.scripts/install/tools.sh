9d43696d854a614316ecc8dbf075609027c755bb
Gertjan Rolink <gertjan.rolink@outlook.com>
2024-04-16 21:10:20 +0200

    nvim as local user

--- scripts/.scripts/install/tools.sh
+++ scripts/.scripts/install/tools.sh
@@ -108,8 +108,8 @@ function install-pythontools() {
 ###############################################################################
 function install-neovim() {
     echo "Install Neovim"
-    sudo curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o /usr/bin/nvim
-    sudo chmod 755 /usr/bin/nvim
+    curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/.local/bin/nvim
+    chmod u+x ~/.local/bin/nvim
     echo ''
 }
 
