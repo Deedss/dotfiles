f3be3e5dfb250cb83f59fb72d5c2c1f0c321120b
Deedss <gertjan.rolink@outlook.com>
2024-05-05 08:37:05 +0200

    Add bat and eza

--- scripts/.scripts/install/fedora.sh
+++ scripts/.scripts/install/fedora.sh
@@ -198,7 +198,7 @@ function default-packages(){
     sudo dnf install -y openssl zstd ncurses git ripgrep \
         ncurses-libs stow zsh util-linux-user \
         java-17-openjdk java-17-openjdk-devel jetbrains-mono-fonts google-roboto-fonts \
-        steam-devices wl-clipboard
+        steam-devices wl-clipboard bat eza
 }
 
 ###############################################################################
