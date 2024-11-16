1dc05e33e3f9d75cb4273567927b14598194fa18
Deedss <gertjan.rolink@outlook.com>
2024-11-16 17:04:30 +0100

    update vscode.lua

--- nvim/.config/nvim/vscode.lua
+++ nvim/.config/nvim/vscode.lua
@@ -113,8 +113,8 @@ vim.keymap.set({ "n", "x", "i" }, "gL",
   function() vscode.with_insert(function() vscode.action("editor.action.addSelectionToPreviousFindMatch") end) end)
 
 -- -- Treesitter Expand Shrink
-vim.keymap.set({ 'n', 'x' }, '[x', [[<cmd>lua require('vscode').call('editor.action.smartSelect.expand')<cr>]])
-vim.keymap.set('x', ']x', [[<cmd>lua require('vscode').call('editor.action.smartSelect.shrink')<cr>]])
+-- vim.keymap.set({ 'n', 'x' }, '[x', [[<cmd>lua require('vscode').call('editor.action.smartSelect.expand')<cr>]])
+-- vim.keymap.set('x', ']x', [[<cmd>lua require('vscode').call('editor.action.smartSelect.shrink')<cr>]])
 
 ---------------------------------------------------------------------------
 --- AUTOCOMMANDS                                                        ---
@@ -170,6 +170,49 @@ require('lazy').setup({
       config = function()
         require("nvim-surround").setup({})
       end,
+    },
+    { -- Highlight, edit, and navigate code
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+        ---@diagnostic disable-next-line: missing-fields
+        require("nvim-treesitter.configs").setup({
+          ensure_installed = "all",
+          -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+          auto_install = true,
+          highlight = { enable = true },
+          indent = { enable = true },
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = "v",
+              node_incremental = "v",
+              scope_incremental = false,
+              node_decremental = "V",
+            },
+          },
+        })
+      end,
+    },
+    {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      after = "nvim-treesitter",
+      requires = "nvim-treesitter/nvim-treesitter",
+      event = "VeryLazy",
+      enabled = true,
+      config = function()
+        require 'nvim-treesitter.configs'.setup({
+          textobjects = {
+            move = {
+              enable = true,
+              goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+              goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+              goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+              goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+            },
+          },
+        })
+      end
     }
   },
 })
