43fd00d7f824e28c6d96d719daa2940a035bde2b
Deedss <gertjan.rolink@outlook.com>
2025-05-18 18:49:01 +0200

    update scripts

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -1,16 +1,16 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = {
-    "BufWritePost", "BufReadPost", "InsertLeave"
-  },
-  config = function()
-    local lint = require("lint")
+    "mfussenegger/nvim-lint",
+    event = {
+        "BufWritePost", "BufReadPost", "InsertLeave"
+    },
+    config = function()
+        local lint = require("lint")
 
-    lint.linters_by_ft = {
-      c = { "clangtidy" },
-      cpp = { "clangtidy" },
-      cmake = { "cmakelint" },
-      python = { "ruff" },
-    }
-  end,
+        lint.linters_by_ft = {
+            c = { "clangtidy" },
+            cpp = { "clangtidy" },
+            cmake = { "cmakelint" },
+            python = { "ruff" },
+        }
+    end,
 }
