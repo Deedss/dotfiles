1c6d4db2e69044dcb43e11ff4aac9b5fa500af4d
Deedss <gertjan.rolink@outlook.com>
2025-06-17 21:52:15 +0200

    update nvim, kitty and vscodevim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,8 +7,14 @@ return {
       require("catppuccin").setup({
         flavour = "macchiato",
       })
-      vim.cmd([[colorscheme catppuccin]])
+      -- vim.cmd([[colorscheme catppuccin]])
+    end
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      vim.cmd([[colorscheme onedark]])
     end
   }
-
 }
