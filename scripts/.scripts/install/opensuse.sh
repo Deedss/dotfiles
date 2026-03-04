cb4dd7053d61acb4ac9b75e39d013a70a05066a6
Deedss <gertjan.rolink@outlook.com>
2026-03-04 05:39:31 +0100

    add opensuse

--- scripts/.scripts/install/opensuse.sh
+++ scripts/.scripts/install/opensuse.sh
@@ -123,8 +123,9 @@ install-arc-theme() {
 ###############################################################################
 install-vscode() {
     echo "Install Visual Studio Code"
-    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/zypp/repos.d/vscode.repo > /dev/null
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && \
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" \ 
+    | sudo tee /etc/zypp/repos.d/vscode.repo > /dev/null
 
     sudo zypper install code
 }
