a89350a3307ff453a248b3dd7c5eb6cfd4d00270
Deedss <gertjan.rolink@outlook.com>
2026-04-12 12:10:02 +0200

    cleaned up treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,10 @@
-return { -- Highlight, edit, and navigate code
-  {      -- Highlight, edit, and navigate code
+return {
+  {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     lazy = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter").setup({
-        ignore_install = { "hoon", "ipkg" },
-        auto_install = true,
-        highlight = { enable = true },
-        folds = { enable = true },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "]x",
-            node_incremental = "]x",
-            scope_incremental = false,
-            node_decremental = "[x",
-          },
-        },
-      })
       require('nvim-treesitter').install('all')
     end,
   },
