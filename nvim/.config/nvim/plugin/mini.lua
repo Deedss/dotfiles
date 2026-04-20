6c2c93b180823805308864f4383fe70c33c0aeba
Deedss <gertjan.rolink@outlook.com>
2026-04-20 21:55:57 +0200

    add mini.files

--- nvim/.config/nvim/plugin/mini.lua
+++ nvim/.config/nvim/plugin/mini.lua
@@ -3,8 +3,12 @@ vim.pack.add {
   { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
 }
 
+require('mini.icons').setup()
 require('mini.ai').setup()
 require('mini.diff').setup()
+require('mini.files').setup()
 require('mini.move').setup()
 require('mini.statusline').setup()
 require('mini.tabline').setup()
+
+vim.keymap.set('n', '-', function() MiniFiles.open() end, { desc = 'MiniFiles Open' })
