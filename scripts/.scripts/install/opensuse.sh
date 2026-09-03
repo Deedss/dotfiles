5e2c44f640a6a3678b20e4ce701455e683d4eb40
Deedss <gertjan.rolink@outlook.com>
2026-09-03 19:22:40 +0200

    add zram

--- scripts/.scripts/install/opensuse.sh
+++ scripts/.scripts/install/opensuse.sh
@@ -137,4 +137,6 @@ install-brave() {
 install-zram() {
     sudo zypper install -y zram-generator
     echo -e "[zram0]\nzram-size = min(ram, 8192)" | sudo ktee /etc/systemd/zram-generator.conf
+    sudo systemctl daemon-reload
+    sudo systemctl start systemd-zram-setup@zram0.service
 }
