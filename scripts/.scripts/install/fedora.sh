a57f47951f6a904e4d94c02c6a11960f36b93798
Deedss <gertjan.rolink@outlook.com>
2026-09-04 12:08:13 +0200

    update scripts

--- scripts/.scripts/install/fedora.sh
+++ scripts/.scripts/install/fedora.sh
@@ -19,7 +19,6 @@ install-desktop() {
     install-rust
     install-python-tools
     install-zed
-    install-neovim
     install-cli-tools
     install-kitty
 
@@ -124,7 +123,7 @@ install-default-packages() {
         util-linux-user java-25-openjdk java-25-openjdk-devel \
         jetbrains-mono-fonts google-roboto-fonts steam-devices \
         wl-clipboard nodejs lsd bat zoxide fd-find procs ripgrep \
-        kcalc okular gwenview plasma-milou vim
+        kcalc okular gwenview plasma-milou vim fzf neovim
 
     ### Podman
     sudo dnf install -y podman podman-compose podman-docker buildah distrobox
